33985087e481a6c58cc894ae40f58c1be627102e
naatii <natiworlds@gmail.com>
2023-12-02 12:57:54 +0100

    regalo día 1

--- js/main.js
+++ js/main.js
@@ -1,95 +1,129 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const currentDate = new Date();
-    const currentMonth = currentDate.getMonth() + 1; // Devuelve un valor de 0 a 11, por lo que se suma 1.
+  const currentDate = new Date();
+  const currentMonth = currentDate.getMonth() + 1; // Devuelve un valor de 0 a 11, por lo que se suma 1.
 
-    if (currentMonth === 12) {
-      const calendar = document.getElementById('calendar');
+  if (currentMonth === 12) {
+    const calendar = document.getElementById('calendar');
 
-      for (let i = 1; i <= 31; i++) {
-        const day = document.createElement('div');
-        day.classList.add('day');
+    for (let i = 1; i <= 31; i++) {
+      const day = document.createElement('div');
+      day.classList.add('day');
 
-        if (i > currentDate.getDate()) {
-          day.classList.add('locked');
-          const daysUntilUnlock = i - currentDate.getDate();
-          day.innerHTML = `<button class="counter">${daysUntilUnlock} días</button>`;
-        } else {
-          // Contenido desbloqueado
-          day.innerHTML = `Día ${i}`;
-          // Aquí puedes agregar el contenido específico para cada día
-          if (i === 1) {
-            day.onclick = () => {
-                const regalo = document.createElement('p');
-                regalo.textContent = 'Ejemplo de regalo: Un pequeño dulce.';
-                day.appendChild(regalo);
+      if (i > currentDate.getDate()) {
+        day.classList.add('locked');
+        const daysUntilUnlock = i - currentDate.getDate();
+        day.innerHTML = `<button class="counter">${daysUntilUnlock} días</button>`;
+      } else {
+        // Contenido desbloqueado
+        day.innerHTML = `Día ${i}`;
+        // Aquí puedes agregar el contenido específico para cada día
+        if (i === 1) {
+          day.onclick = () => {
+            const existingRegalo = day.querySelector('.regalo1');
+        
+            if (existingRegalo) {
+              // Si ya existe el regalo, elimínalo
+              existingRegalo.remove();
+            } else {
+              // Si no existe, crea el regalo
+              const regalo = document.createElement('p');
+              const regalo1 = document.createElement('a');
+              regalo1.setAttribute('href', 'regalo1/regalo1.html');
+              regalo1.setAttribute('target', '_blank');
+              regalo1.classList.add('regalo1');
+              regalo.textContent = '> Regalo día 1.';
+              regalo1.appendChild(regalo);
+              day.appendChild(regalo1);
             }
           }
         }
-
-        calendar.appendChild(day);
+        
+        if (i === 2) {
+          day.onclick = () => {
+            const existingRegalo = day.querySelector('.regalo1');
+        
+            if (existingRegalo) {
+              // Si ya existe el regalo, elimínalo
+              existingRegalo.remove();
+            } else {
+              // Si no existe, crea el regalo
+              const regalo = document.createElement('p');
+              const regalo2 = document.createElement('a');
+              regalo2.setAttribute('href', 'regalo2/regalo2.html');
+              regalo2.setAttribute('target', '_blank');
+              regalo2.classList.add('regalo1');
+              regalo.textContent = '> Regalo día 2.';
+              regalo2.appendChild(regalo);
+              day.appendChild(regalo2);
+            }
+          }
+        }
+        
       }
-    } else {
-        // Mensaje para meses que no son diciembre
-        const message = document.createElement('p');
-        message.textContent = 'El calendario de adviento está disponible solo en diciembre.';
-        document.body.appendChild(message);
-        // para mientras espera   
-        const esperar = document.createElement('section');
-        const tituloSections = document.createElement('h2');
-        const pSections = document.createElement('p');
-        tituloSections.textContent = 'Y para mientras esperas...';
-        pSections.textContent = 'Puedes jugas a algunos jueguitos';
-        // Los juegos a introducir
-        const juegos = document.createElement('div');
-        juegos.classList.add('juegos');
-        // Enlaces a los juegos
-        
-        // enlace juego boton
-        const enlaceJuegoBoton = document.createElement('a');
-        enlaceJuegoBoton.setAttribute('href', 'https://naatii.github.io/JuegoBoton/');
-        enlaceJuegoBoton.setAttribute('target', '_blank');
-        
-        // enlace juego rooted war
-        const enlaceJuegoRooted = document.createElement('a');
-        enlaceJuegoRooted.setAttribute('href', 'https://gaulent.itch.io/rooted-war');
-        enlaceJuegoRooted.setAttribute('target', '_blank');
-        
-        // enlace juego tikpocalipsis
-        
-        const enlaceJuegoTikpocalipsis = document.createElement('a');
-        enlaceJuegoTikpocalipsis.setAttribute('href', 'https://alexroivas.itch.io/tikpocalipsis');
-        enlaceJuegoTikpocalipsis.setAttribute('target', '_blank');
-        
-        // juego del botón
-        const juegoBoton = document.createElement('img');
-        juegoBoton.setAttribute('src', 'img/juegoBoton.png');
-        juegoBoton.setAttribute('alt', 'imagen del juego del botón');
-        juegoBoton.classList.add('juegoImg');
-        enlaceJuegoBoton.appendChild(juegoBoton);
 
-        // Juego rooted War
-        const juegoRooted = document.createElement('img');
-        juegoRooted.setAttribute('src', 'img/juegoRooted.webp');
-        juegoRooted.setAttribute('alt', 'imagen del juego del botón');
-        juegoRooted.classList.add('juegoImg');
-        enlaceJuegoRooted.appendChild(juegoRooted);
-        
-        // juego tikpocalipsis
-        const juegoTikpocalipsis = document.createElement('img');
-        juegoTikpocalipsis.setAttribute('src', 'img/juegoTikpocalipsis.webp');
-        juegoTikpocalipsis.setAttribute('alt', 'imagen del juego del botón');
-        juegoTikpocalipsis.classList.add('juegoImg');
-        enlaceJuegoTikpocalipsis.appendChild(juegoTikpocalipsis);
-        
-
-        // Añadiendo todo al html
-        document.body.appendChild(esperar);
-        esperar.appendChild(tituloSections);
-        esperar.appendChild(pSections);
-        juegos.appendChild(enlaceJuegoBoton);
-        juegos.appendChild(enlaceJuegoRooted);
-        juegos.appendChild(enlaceJuegoTikpocalipsis);
-        esperar.appendChild(juegos);
+      calendar.appendChild(day);
     }
-    
-  });
+  } else {
+    // Mensaje para meses que no son diciembre
+    const message = document.createElement('p');
+    message.textContent = 'El calendario de adviento está disponible solo en diciembre.';
+    document.body.appendChild(message);
+
+    // para mientras espera
+    const esperar = document.createElement('section');
+    const tituloSections = document.createElement('h2');
+    const pSections = document.createElement('p');
+    tituloSections.textContent = 'Y para mientras esperas...';
+    pSections.textContent = 'Puedes jugar a algunos jueguitos';
+    // Los juegos a introducir
+    const juegos = document.createElement('div');
+    juegos.classList.add('juegos');
+    // Enlaces a los juegos
+
+    // enlace juego boton
+    const enlaceJuegoBoton = document.createElement('a');
+    enlaceJuegoBoton.setAttribute('href', 'https://naatii.github.io/JuegoBoton/');
+    enlaceJuegoBoton.setAttribute('target', '_blank');
+
+    // enlace juego rooted war
+    const enlaceJuegoRooted = document.createElement('a');
+    enlaceJuegoRooted.setAttribute('href', 'https://gaulent.itch.io/rooted-war');
+    enlaceJuegoRooted.setAttribute('target', '_blank');
+
+    // enlace juego tikpocalipsis
+
+    const enlaceJuegoTikpocalipsis = document.createElement('a');
+    enlaceJuegoTikpocalipsis.setAttribute('href', 'https://alexroivas.itch.io/tikpocalipsis');
+    enlaceJuegoTikpocalipsis.setAttribute('target', '_blank');
+
+    // juego del botón
+    const juegoBoton = document.createElement('img');
+    juegoBoton.setAttribute('src', 'img/juegoBoton.png');
+    juegoBoton.setAttribute('alt', 'imagen del juego del botón');
+    juegoBoton.classList.add('juegoImg');
+    enlaceJuegoBoton.appendChild(juegoBoton);
+
+    // Juego rooted War
+    const juegoRooted = document.createElement('img');
+    juegoRooted.setAttribute('src', 'img/juegoRooted.webp');
+    juegoRooted.setAttribute('alt', 'imagen del juego del botón');
+    juegoRooted.classList.add('juegoImg');
+    enlaceJuegoRooted.appendChild(juegoRooted);
+
+    // juego tikpocalipsis
+    const juegoTikpocalipsis = document.createElement('img');
+    juegoTikpocalipsis.setAttribute('src', 'img/juegoTikpocalipsis.webp');
+    juegoTikpocalipsis.setAttribute('alt', 'imagen del juego del botón');
+    juegoTikpocalipsis.classList.add('juegoImg');
+    enlaceJuegoTikpocalipsis.appendChild(juegoTikpocalipsis);
+
+    // Añadiendo todo al html
+    document.body.appendChild(esperar);
+    esperar.appendChild(tituloSections);
+    esperar.appendChild(pSections);
+    juegos.appendChild(enlaceJuegoBoton);
+    juegos.appendChild(enlaceJuegoRooted);
+    juegos.appendChild(enlaceJuegoTikpocalipsis);
+    esperar.appendChild(juegos);
+  }
+});
---
+++ regalo1/regalo1.js
@@ -0,0 +1,40 @@
+const palabras = {
+    palabra1: "9",
+    palabra2: "febrero",
+    palabra3: "amigas",
+    palabra4: "hermanas",
+    palabra5: "relación",
+    palabra6: "más",
+    palabra7: "twitch",
+    palabra8: "discord",
+    palabra9: "ojos",
+    palabra10: "te quiero",
+    palabra11: "te amo",
+    palabra12: "SIEMPRE",
+    palabra13: "JUNTAS",
+  };
+
+  document.addEventListener('DOMContentLoaded', function () {
+    const resultadoElemento = document.getElementById('resultado');
+
+    for (const key in palabras) {
+      if (palabras.hasOwnProperty(key)) {
+        const palabraIncompleta = document.getElementById(key);
+        const palabraCompleta = palabras[key];
+
+        const input = document.createElement('input');
+        input.setAttribute('type', 'text');
+        input.setAttribute('maxlength', palabraCompleta.length);
+        palabraIncompleta.appendChild(input);
+
+        input.addEventListener('input', function () {
+          const valorInput = this.value.toLowerCase();
+          if (valorInput === palabraCompleta.toLowerCase()) {
+            resultadoElemento.textContent = '¡Correcto!';
+          } else {
+            resultadoElemento.textContent = '';
+          }
+        });
+      }
+    }
+  });
