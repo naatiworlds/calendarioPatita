7e2c8e8f6d4d183d5bbd05bdb925d11a6d47926c
naatii <natiworlds@gmail.com>
2023-12-07 13:32:40 +0100

    regalo día 4

--- js/main.js
+++ js/main.js
@@ -78,6 +78,26 @@ document.addEventListener('DOMContentLoaded', function () {
             }
           }
         }
+        if (i === 4) {
+          day.onclick = () => {
+            const existingRegalo = day.querySelector('.regalo1');
+        
+            if (existingRegalo) {
+              // Si ya existe el regalo, elimínalo
+              existingRegalo.remove();
+            } else {
+              // Si no existe, crea el regalo
+              const regalo = document.createElement('p');
+              const regalo4 = document.createElement('a');
+              regalo4.setAttribute('href', 'regalo4/regalo4.html');
+              regalo4.setAttribute('target', '_blank');
+              regalo4.classList.add('regalo1');
+              regalo.textContent = '> Regalo día 4.';
+              regalo4.appendChild(regalo);
+              day.appendChild(regalo4);
+            }
+          }
+        }
         
       }
 
---
+++ regalo4/regalo4.js
@@ -0,0 +1,118 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const queryParams = new URLSearchParams(window.location.search);
+    const selectedLevel = queryParams.get('level');
+
+    const words = {
+        easy: ['patita', 'te amo', 'natilla', 'melona'],
+        medium: ['marmota', 'dormilona', 'abasho', 'siempre', 'juntas'],
+        hard: ['buenoooss diaaass'],
+    };
+
+    let selectedWord = '';
+    let guessedLetters = [];
+    let mistakes = 0;
+    let maxMistakes = 6;
+
+    const hangmanParts = document.querySelectorAll('.figure-part');
+    const wordContainer = document.getElementById('word-container');
+    const keyboardContainer = document.getElementById('keyboard-container');
+    const messageContainer = document.getElementById('message-container');
+
+    function chooseWord(difficulty) {
+        const wordList = words[difficulty];
+        return wordList[Math.floor(Math.random() * wordList.length)];
+    }
+
+    function displayWord() {
+        wordContainer.innerHTML = '';
+        selectedWord.split('').forEach((letter) => {
+            const letterContainer = document.createElement('div');
+            letterContainer.classList.add('word-letter');
+            if (guessedLetters.includes(letter)) {
+                letterContainer.textContent = letter;
+            }
+            wordContainer.appendChild(letterContainer);
+        });
+    }
+
+    function displayKeyboard() {
+        keyboardContainer.innerHTML = '';
+        const alphabet = 'abcdefghijklmnopqrstuvwxyz ';
+        alphabet.split('').forEach((letter) => {
+            const key = document.createElement('div');
+            key.classList.add('key');
+            key.textContent = letter;
+            key.addEventListener('click', () => guessLetter(letter));
+            keyboardContainer.appendChild(key);
+        });
+    }
+
+    function guessLetter(letter) {
+        if (!guessedLetters.includes(letter)) {
+            guessedLetters.push(letter);
+            if (!selectedWord.includes(letter)) {
+                mistakes++;
+                updateHangman();
+            }
+        }
+
+        displayWord();
+        checkGameStatus();
+    }
+
+    function updateHangman() {
+        if (mistakes <= maxMistakes) {
+            hangmanParts[mistakes - 1].style.display = 'initial';
+        } else {
+            endGame(false);
+        }
+    }
+
+    function checkGameStatus() {
+        if (!wordContainer.textContent.includes('_')) {
+            endGame(true);
+        }
+    }
+
+    function endGame(isWinner) {
+        if (isWinner && wordContainer.textContent === selectedWord) {
+            const message = document.createElement('p');
+            message.textContent = '¡Ganaste! 😃';
+            messageContainer.innerHTML = '';
+            messageContainer.appendChild(message);
+
+            const restartButton = document.createElement('button');
+            restartButton.textContent = 'Reiniciar Juego';
+            restartButton.addEventListener('click', () => startGame());
+            messageContainer.appendChild(restartButton);
+        }
+        else if (!isWinner && mistakes >= maxMistakes) {
+            const message = document.createElement('p');
+            message.textContent = 'Perdiste 😢';
+            messageContainer.innerHTML = '';
+            messageContainer.appendChild(message);
+
+            const restartButton = document.createElement('button');
+            restartButton.textContent = 'Reiniciar Juego';
+            restartButton.addEventListener('click', () => startGame());
+            messageContainer.appendChild(restartButton);
+        }
+    }
+
+
+    function startGame() {
+        selectedWord = chooseWord(selectedLevel);
+        guessedLetters = [];
+        mistakes = 0;
+
+        hangmanParts.forEach((part) => {
+            part.style.display = 'none';
+        });
+
+        displayWord();
+        displayKeyboard();
+        messageContainer.innerHTML = '';
+    }
+
+    startGame(); // Llamada inicial al iniciar el juego
+});
