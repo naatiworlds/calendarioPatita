1a05eef40da8a09be6e4ccd0aa4ee38301f0d21f
naatii <natiworlds@gmail.com>
2023-12-28 22:42:53 +0100

    Día 9 arreglado

--- js/main.js
+++ js/main.js
@@ -168,13 +168,33 @@ document.addEventListener('DOMContentLoaded', function () {
             } else {
               // Si no existe, crea el regalo
               const regalo = document.createElement('p');
-              const regalo8 = document.createElement('a');
+              const regalo9 = document.createElement('a');
               regalo8.setAttribute('href', 'regalo8/regalo8.html');
               regalo8.setAttribute('target', '_blank');
               regalo8.classList.add('regalo1');
               regalo.textContent = '> Regalo día 8.';
               regalo8.appendChild(regalo);
               day.appendChild(regalo8);
+            }
+          }
+        }
+        if (i === 9) {
+          day.onclick = () => {
+            const existingRegalo = day.querySelector('.regalo1');
+        
+            if (existingRegalo) {
+              // Si ya existe el regalo, elimínalo
+              existingRegalo.remove();
+            } else {
+              // Si no existe, crea el regalo
+              const regalo = document.createElement('p');
+              const regalo9 = document.createElement('a');
+              regalo9.setAttribute('href', 'regalo9/regalo9.html');
+              regalo9.setAttribute('target', '_blank');
+              regalo9.classList.add('regalo1');
+              regalo.textContent = '> Regalo día 9.';
+              regalo9.appendChild(regalo);
+              day.appendChild(regalo9);
             }
           }
         }
